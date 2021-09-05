583d0444770d2683ec08deade50b774496f1df1d
HalReal <85908977+HalReal@users.noreply.github.com>
2021-09-04 21:23:49 -0600

    Add files via upload

---
+++ Producto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Enums;
+using Newtonsoft.Json;
+
+
+namespace Domain
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    
+    public class Producto
+    {
+       [JsonProperty] public int Codigo { get; set; }
+       [JsonProperty] public string Nombre { get; set; }
+       [JsonProperty] public string Descripcion { get; set; }
+       [JsonProperty] public int Cantidad { get; set; }
+       [JsonProperty] public decimal Precio { get; set; }
+       [JsonProperty] public DateTime Caducidad { get; set; }
+       [JsonProperty] public UnidadMedida UnidadMedida { get; set; }
+
+    }
+}
